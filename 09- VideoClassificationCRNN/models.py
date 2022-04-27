1052def28b2dca496272bf63e3e418876b83ee1c
Nahid Ebrahimian <nsebrahimian@gmail.com>
2022-04-27 23:57:13 +0430

    change backbone model

--- 09- VideoClassificationCRNN/models.py
+++ 09- VideoClassificationCRNN/models.py
@@ -1,71 +1,77 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, SimpleRNN, GRU, LSTM, Dense, Flatten, TimeDistributed
-
-# RNN Model
-
-def RNN_model(max_seq_len):
-
-    frame_width = 112
-    frame_height = 112
-
-    frame_input = tf.keras.Input((None, frame_height, frame_width, 3))
-    mask_input = tf.keras.Input((max_seq_len,), dtype="bool")
-
-    conv = TimeDistributed(Conv2D(16, (3, 3), activation="relu", padding="same"))(frame_input)
-    maxpool = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv)
-    flatten = TimeDistributed(Flatten())(maxpool)
-                                        
-    rnn = SimpleRNN(64, return_sequences=True)(flatten, mask=mask_input)  
-    rnn = SimpleRNN(32)(rnn)     
-    output = Dense(2, activation="softmax")(rnn)
-
-    rnn_model = tf.keras.Model([frame_input, mask_input], output)
-
-    return rnn_model
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, SimpleRNN, GRU, LSTM, Dense, Flatten, TimeDistributed
 
 
+class Models():
 
-  # GRU Model
+      def __init__(self, input_shape, max_seq_len):
+        self.frame_height = input_shape[0]
+        self.frame_width = input_shape[1]
+        self.max_seq_len = max_seq_len
 
-def GRU_model(max_seq_len):
-
-    frame_width = 112
-    frame_height = 112
-
-    frame_input = tf.keras.Input((None, frame_height, frame_width, 3))
-    mask_input = tf.keras.Input((max_seq_len,), dtype="bool")
-
-    conv = TimeDistributed(Conv2D(16, (3, 3), activation="relu", padding="same"))(frame_input)
-    maxpool = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv)
-    flatten = TimeDistributed(Flatten())(maxpool)
-                                        
-    gru = GRU(64, return_sequences=True)(flatten, mask=mask_input)  
-    gru = GRU(32)(gru)     
-    output = Dense(2, activation="softmax")(gru)
-
-    gru_model = tf.keras.Model([frame_input, mask_input], output)
-
-    return gru_model
+        self.frame_input = tf.keras.Input((None, self.frame_height, self.frame_width, 3))
+        self.mask_input = tf.keras.Input((self.max_seq_len,), dtype="bool")
 
 
-# LSTM Model
+      # Backbone Model(feature extractor)
+      def backbone_model(self):
 
-def LSTM_model(max_seq_len):
+        feature_extractor = tf.keras.applications.ResNet50V2(
+          include_top=False,
+          weights="imagenet",
+          input_tensor=None,
+          input_shape=(self.frame_height, self.frame_width, 3),
+          pooling='avg',
+          classes=2,
+          classifier_activation="softmax")
+        
+        for layer in feature_extractor.layers: # Freeze the layers
+          layer.trainable=False
 
-    frame_width = 112
-    frame_height = 112
+        return feature_extractor
 
-    frame_input = tf.keras.Input((None, frame_height, frame_width, 3))
-    mask_input = tf.keras.Input((max_seq_len,), dtype="bool")
 
-    conv = TimeDistributed(Conv2D(16, (3, 3), activation="relu", padding="same"))(frame_input)
-    maxpool = TimeDistributed(MaxPooling2D(pool_size=(2, 2)))(conv)
-    flatten = TimeDistributed(Flatten())(maxpool)
-                                        
-    lstm = GRU(64, return_sequences=True)(flatten, mask=mask_input)  
-    lstm = GRU(32)(lstm)     
-    output = Dense(2, activation="softmax")(lstm)
+      # RNN Model
+      def RNN_model(self):
 
-    lstm_model = tf.keras.Model([frame_input, mask_input], output)
+          features = TimeDistributed(self.backbone_model())(self.frame_input)
+          flatten = TimeDistributed(Flatten())(features)
+          droupout = Dropout(0.4)(flatten)
 
-    return lstm_model
+          rnn = SimpleRNN(64)(droupout, mask=self.mask_input)                                       
+          output = Dense(2, activation="softmax")(rnn)
+
+          rnn_model = tf.keras.Model([self.frame_input, self.mask_input], output)
+
+          return rnn_model
+
+
+
+      # GRU Model
+      def GRU_model(self):
+
+          features = TimeDistributed(self.backbone_model())(self.frame_input)
+          flatten = TimeDistributed(Flatten())(features)
+          droupout = Dropout(0.4)(flatten)
+
+          gru = GRU(64)(droupout, mask=self.mask_input)                            
+          output = Dense(2, activation="softmax")(gru)
+
+          gru_model = tf.keras.Model([self.frame_input, self.mask_input], output)
+
+          return gru_model
+
+
+      # LSTM Model
+      def LSTM_model(self):
+
+          features = TimeDistributed(self.backbone_model())(self.frame_input)
+          flatten = TimeDistributed(Flatten())(features)
+          droupout = Dropout(0.4)(flatten)
+
+          lstm = LSTM(64)(droupout, mask=self.mask_input)                                       
+          output = Dense(2, activation="softmax")(lstm)
+
+          lstm_model = tf.keras.Model([self.frame_input, self.mask_input], output)
+
+          return lstm_model
